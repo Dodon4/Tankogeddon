aac338691654e1badc2b04f3a1e80985e652cf3d
Alexandr Dodonov <dodon19980404@yandex.ru>
2021-08-22 23:35:05 +0300

    Lesson Code

--- Source/Tankogeddon/TankPawn.cpp
+++ Source/Tankogeddon/TankPawn.cpp
@@ -5,6 +5,12 @@
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/ArrowComponent.h"
+
+#include "Tankogeddon.h"
+#include "TankPlayerController.h"
+#include "Cannon.h"
 // Sets default values
 ATankPawn::ATankPawn()
 {
@@ -16,6 +22,9 @@ ATankPawn::ATankPawn()
 
 	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Tank turret"));
 	TurretMesh->SetupAttachment(BodyMesh);
+
+	CannonSetupPoint = CreateDefaultSubobject<UArrowComponent>(TEXT("Cannon setup point"));
+	CannonSetupPoint->AttachToComponent(TurretMesh, FAttachmentTransformRules::KeepRelativeTransform);
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring arm"));
 	SpringArm->SetupAttachment(BodyMesh);
@@ -32,26 +41,75 @@ void ATankPawn::MoveForward(float AxisValue)
 {
 	TargetForwardAxisValue = AxisValue;
 }
-void ATankPawn::MoveRight(float AxisValue)
-{
-	TargetRightAxisValue = AxisValue;
-}
+//void ATankPawn::MoveRight(float AxisValue)
+//{
+//	TargetRightAxisValue = AxisValue;
+//}
 // Called when the game starts or when spawned
+void ATankPawn::SetupCannon()
+{
+	if (Cannon)
+	{
+		Cannon->Destroy();
+		Cannon = nullptr;
+	}
+
+	FActorSpawnParameters Params;
+	Params.Instigator = this;
+	Params.Owner = this;
+	Cannon = GetWorld()->SpawnActor<ACannon>(CannonClass, Params);
+	Cannon->AttachToComponent(CannonSetupPoint, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+}
+
+void ATankPawn::Fire()
+{
+	if (Cannon)
+	{
+		Cannon->Fire();
+	}
+}
+
 void ATankPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
+	TankController = Cast<ATankPlayerController>(GetController());
 
+	SetupCannon();
+}
+void ATankPawn::RotateRight(float AxisValue)
+{
+	TargetRightAxisValue = AxisValue;
+}
 // Called every frame
 void ATankPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	// Movemevt
 	FVector CurrentLocation = GetActorLocation();
 	FVector ForwardVector = GetActorForwardVector();
-	FVector RightVector = GetActorRightVector();
-	FVector MovePosition = CurrentLocation + ForwardVector * MoveSpeed * TargetForwardAxisValue * DeltaTime
-		+ RightVector * MoveSpeed * TargetRightAxisValue * DeltaTime;
+	//FVector RightVector = GetActorRightVector();
+	FVector MovePosition = CurrentLocation + ForwardVector * MoveSpeed * TargetForwardAxisValue * DeltaTime;
+		//+ RightVector * MoveSpeed * TargetRightAxisValue * DeltaTime;
 	SetActorLocation(MovePosition, true);
+
+	//Rotation
+	CurrentRightAxisValue = FMath::Lerp(CurrentRightAxisValue, TargetRightAxisValue, InterpolationKey);
+	float yawRotation = RotationSpeed * CurrentRightAxisValue * DeltaTime;
+	FRotator currentRotation = GetActorRotation();
+
+	yawRotation = currentRotation.Yaw + yawRotation;
+
+	FRotator newRotation = FRotator(0, yawRotation, 0);
+	SetActorRotation(newRotation);
+
+	//Turret rotation
+	if (TankController)
+	{
+		FVector mousePos = TankController->GetMousePos();
+		FRotator targetRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), mousePos);
+		FRotator currRotation = TurretMesh->GetComponentRotation();
+		targetRotation.Pitch = currRotation.Pitch;
+		targetRotation.Roll = currRotation.Roll;
+		TurretMesh->SetWorldRotation(FMath::Lerp(currRotation, targetRotation, TurretRotationInterpolationKey));
+	}
 }
--- Source/Tankogeddon/TankPlayerController.cpp
+++ Source/Tankogeddon/TankPlayerController.cpp
@@ -3,16 +3,33 @@
 
 #include "TankPlayerController.h"
 #include "TankPawn.h"
+#include "DrawDebugHelpers.h"
 ATankPlayerController::ATankPlayerController()
 {
-
+	bShowMouseCursor = true;
 }
 
 void ATankPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
 	InputComponent->BindAxis("MoveForward", this, &ATankPlayerController::MoveForward);
-	InputComponent->BindAxis("MoveRight", this, &ATankPlayerController::MoveRight);
+//	InputComponent->BindAxis("MoveRight", this, &ATankPlayerController::MoveRight);
+	InputComponent->BindAxis("RotateRight", this, &ATankPlayerController::RotateRight);
+	InputComponent->BindAction("Fire", IE_Pressed, this, &ATankPlayerController::Fire);
+
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	FVector mouseDirection;
+	DeprojectMousePositionToWorld(MousePos, mouseDirection);
+	FVector pawnPos = TankPawn->GetActorLocation();
+	MousePos.Z = pawnPos.Z;
+	FVector dir = MousePos - pawnPos;
+	dir.Normalize();
+	MousePos = pawnPos + dir * 1000;
+
+	DrawDebugLine(GetWorld(), pawnPos, MousePos, FColor::Green, false, 0.1f, 0, 5);
 }
 
 void ATankPlayerController::BeginPlay()
@@ -27,7 +44,15 @@ void ATankPlayerController::MoveForward(float AxisValue)
 	TankPawn->MoveForward(AxisValue);
 }
 
-void ATankPlayerController::MoveRight(float AxisValue)
+//void ATankPlayerController::MoveRight(float AxisValue)
+//{
+//	TankPawn->MoveRight(AxisValue);
+//}
+void ATankPlayerController::RotateRight(float AxisValue)
 {
-	TankPawn->MoveRight(AxisValue);
+	TankPawn->RotateRight(AxisValue);
+}
+void ATankPlayerController::Fire()
+{
+	TankPawn->Fire();
 }
